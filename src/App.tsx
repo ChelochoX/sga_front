f9c09d7057a48b012c29a0c90b189743e0022858
Cesar Lezcano <cesarlezca@gmail.com>
2025-04-28 16:58:07 -0300

    Primer commit - Proyecto SGA Front

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+
+import React from "react";
+import Login from "./pages/Login";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
